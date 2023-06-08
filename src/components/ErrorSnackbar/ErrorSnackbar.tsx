985db2b136b0cc158d6ea94b73f421c5eaa61f7e
Alexandr Dylevich <dylevich220885@gmail.com>
2023-06-09 00:44:10 +0300

    перешел на useAppSelector и useAppDispatch из rtk в setRequestErrorAC параметры теперь передаются в виде объекта (lesson 17 friday-01 rtk)

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 import Snackbar from '@mui/material/Snackbar'
 import MuiAlert, { AlertProps } from '@mui/material/Alert'
 import {useSelector} from 'react-redux';
-import {AppRootStateType, useAppDispatch, useAppSelector} from '../../app/store';
+
 import {InitialStateType, setRequestErrorAC} from '../../app/appReducer';
+import {useAppDispatch, useAppSelector} from 'app/hooks';
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
     props, ref) {
@@ -13,17 +14,15 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 export const ErrorSnackbar = () => {
     //const [open, setOpen] = useState(true)
     //дефолтный useSelector, который мы типизируем: <весь_стейт, часть_стейта_которую_хотим_достать>
-    const _error = useSelector <AppRootStateType, null | string> ((state)=> state.app.error)
-
     //используем наш типизированный useSelector
-    const error = useAppSelector<null | string>((state)=> state.app.error)
+    const error = useAppSelector((state)=> state.app.error)
     const dispatch = useAppDispatch();
     const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
         if (reason === 'clickaway') {
             return
         }
         //setOpen(false)
-        dispatch(setRequestErrorAC(null))
+        dispatch(setRequestErrorAC({error: null}))
     }
     return (
         <Snackbar open={!!error} autoHideDuration={3000} onClose={handleClose}>
