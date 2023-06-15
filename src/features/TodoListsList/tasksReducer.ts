d829280d9420a999898633f58ad5548e536feb9d
Alexandr Dylevich <dylevich220885@gmail.com>
2023-06-15 22:40:50 +0300

    описал санк fetchTasksTC removeTaskTC через createAsyncThunk, добавил к ним addCase (fulfilled) (lesson 18 friday-02 rtk)

--- src/features/TodoListsList/tasksReducer.ts
+++ src/features/TodoListsList/tasksReducer.ts
@@ -15,14 +15,8 @@ import {
 import {Dispatch} from 'redux';
 import {setRequestErrorAC, SetRequestErrorType, setRequestStatusAC, SetRequestStatusType} from '../../app/appReducer';
 import {handleServerAppError, handleServerNetworkError} from '../../utils/error-utils';
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {RootState} from '../../app/store';
-
-// export type OldTaskType = {
-//     id: string;
-//     title: string;
-//     isDone: boolean;
-// }
 
 const initialState: TasksForTodoListType = {
 	// [todoListId1]: [
@@ -35,6 +29,69 @@ const initialState: TasksForTodoListType = {
 	// ]
 }
 
+//TODO убрать из санок приписку TC
+
+// export const _fetchTasksTC = (todoListId: string) => (dispatch: Dispatch) => {
+// 	dispatch(setRequestStatusAC({status: 'loading'}))
+// 	todoListsApi.getTasks(todoListId)
+// 		.then((res) => {
+// 			dispatch(setTasksAC({todoListId, tasks: res.data.items}))
+// 			dispatch(setRequestStatusAC({status: 'succeeded'}))
+// 		}).catch((e) => {
+// 		handleServerNetworkError(dispatch, e)
+// 	})
+// }
+
+//если санка принимала несколько параметров, то их нужно оборачивать в объект, если же нужен только один параметр, то можно без оборачвания
+
+//thunks
+export const fetchTasksTC = createAsyncThunk (
+	'tasks/fetchTasks',
+	(todoListId: string, thunkAPI) => {
+		const {dispatch} = thunkAPI
+		dispatch(setRequestStatusAC({status: 'loading'}))
+		return todoListsApi.getTasks(todoListId)
+			.then((res) => {
+				const tasks = res.data.items
+				dispatch(setRequestStatusAC({status: 'succeeded'}))
+				return {todoListId, tasks}
+				//можно не создавать промежуточную переменную tasks:
+				//return {todoListId, tasks: res.data.items}
+			})
+			.catch((e) => {handleServerNetworkError(dispatch, e)})
+	}
+)
+
+// export const _removeTaskTC = (todoListId: string, taskForRemoveId: string) => (dispatch: Dispatch<TasksReducerActionsType>) => {
+// 	dispatch(setRequestStatusAC({status: 'loading'}))
+// 	todoListsApi.deleteTask(todoListId, taskForRemoveId)
+// 		.then((res) => {
+// 			if (res.data.resultCode === ResultCode.OK) {
+// 				dispatch(removeTaskAC({todoListId, taskForRemoveId}))
+// 				dispatch(setRequestStatusAC({status: 'succeeded'}))
+// 			} else {
+// 				handleServerAppError(dispatch, res.data)
+// 			}
+// 		}).catch((e) => {
+// 		handleServerNetworkError(dispatch, e)
+// 	})
+// }
+
+//TODO пофиксить то, что убрали if else(убрали т.к. в addCase в экстраРедьюсере падала ошибка, что action.payload может быть undefined
+export const removeTaskTC = createAsyncThunk('tasks/removeTask', (arg: {todoListId: string, taskForRemoveId: string}, thunkAPI) => {
+	const {dispatch} = thunkAPI
+	dispatch(setRequestStatusAC({status: 'loading'}))
+	return todoListsApi.deleteTask(arg.todoListId, arg.taskForRemoveId)
+		.then((res) => {
+			if (res.data.resultCode === ResultCode.OK) {
+				dispatch(setRequestStatusAC({status: 'succeeded'}))
+				return {todoListId: arg.todoListId, taskForRemoveId: arg.taskForRemoveId}
+			} else {
+				handleServerAppError(dispatch, res.data)
+			}
+		})
+})
+
 const slice = createSlice({
 	name: 'tasks',
 	initialState,
@@ -42,15 +99,14 @@ const slice = createSlice({
 		addTaskAC: (state, action: PayloadAction<{ todoListId: string, task: TaskFromServerType }>) => {
 			state[action.payload.todoListId].unshift(action.payload.task)
 		},
-		removeTaskAC: (state, action: PayloadAction<{ todoListId: string, taskForRemoveId: string }>) => {
-			const tasks = state[action.payload.todoListId]
-
-			const index = tasks.findIndex(t => t.id === action.payload.taskForRemoveId)
-			if (index > -1) {
-				//state[action.payload.todoListId].splice(index, 1)
-				tasks.splice(index, 1)
-			}
-		},
+		// removeTaskAC: (state, action: PayloadAction<{ todoListId: string, taskForRemoveId: string }>) => {
+		// 	const tasks = state[action.payload.todoListId]
+		// 	const index = tasks.findIndex(t => t.id === action.payload.taskForRemoveId)
+		// 	if (index > -1) {
+		// 		//state[action.payload.todoListId].splice(index, 1)
+		// 		tasks.splice(index, 1)
+		// 	}
+		// },
 		changeTaskStatusAC: (state, action: PayloadAction<{ todoListId: string, taskForChangeId: string, newStatus: number }>) => {
 			const tasks = state[action.payload.todoListId]
 			const index = tasks.findIndex(t => t.id === action.payload.taskForChangeId)
@@ -67,11 +123,10 @@ const slice = createSlice({
 				tasks[index].title = action.payload.newTitle
 			}
 		},
-		setTasksAC: (state, action: PayloadAction<{ todoListId: string, tasks: TaskFromServerType[] }>) => {
-			state[action.payload.todoListId] = action.payload.tasks
-		},
+		// setTasksAC: (state, action: PayloadAction<{ todoListId: string, tasks: TaskFromServerType[] }>) => {
+		// 	state[action.payload.todoListId] = action.payload.tasks
+		// },
 	},
-
 	//extraReducers может быть как объект со свойствами (ключ-значение), а может быть функция, которая будет "собирать" экстра редьюсеры
 	extraReducers: (builder) => {
 		builder
@@ -86,8 +141,24 @@ const slice = createSlice({
 					state[t.id] = []
 				})
 			})
-
+			.addCase(fetchTasksTC.fulfilled, (state, action) => {
+				if (action.payload){
+					state[action.payload.todoListId] = action.payload.tasks
+				}
+			})
+			.addCase(removeTaskTC.fulfilled, (state, action) => {
+				if (action.payload) {
+					const {todoListId, taskForRemoveId} = action.payload
+					const tasks = state[todoListId]
+						const index = tasks.findIndex(t => t.id === taskForRemoveId)
+						if (index > -1) {
+							//state[action.payload.todoListId].splice(index, 1)
+							tasks.splice(index, 1)
+						}
+				}
+		})
 	}
+	//ключ-значение(просто для примера, что так тоже можно делать)
 	// 	{
 	// 	[addTodoListAC.type]: (state, action: PayloadAction<{ newTodoList: TodoListFromServerType }>) => {
 	// 	},
@@ -98,17 +169,119 @@ const slice = createSlice({
 	// }
 })
 
-
 export const tasksReducer = slice.reducer
 
 //actions
 export const {
 	addTaskAC,
-	removeTaskAC,
+	//removeTaskAC,
 	changeTaskStatusAC,
 	changeTaskTitleAC,
-	setTasksAC
+	//setTasksAC
 } = slice.actions
+
+//thunkCreators:
+
+
+export const addTaskTC = (todoListId: string, title: string) => (dispatch: Dispatch) => {
+	dispatch(setRequestStatusAC({status: 'loading'}))
+	todoListsApi.createTask(todoListId, title)
+		.then((res) => {
+			if (res.data.resultCode === ResultCode.OK) {
+				const task = res.data.data.item
+				const action = addTaskAC({todoListId, task})//можно todoListId не передавать, т.к. он есть в свойствах у таски с сервера
+				dispatch(action)
+				dispatch(setRequestStatusAC({status: 'succeeded'}))
+				dispatch(setRequestStatusAC({status: 'idle'}))
+			} else {
+				handleServerAppError<{ item: TaskFromServerType }>(dispatch, res.data)
+			}
+		}).catch((e) => {
+		handleServerNetworkError(dispatch, e)
+	})
+}
+
+
+
+export const changeTaskStatusTC = (todoListId: string, taskForChangeId: string, newStatus: TaskStatuses) =>
+	(dispatch: Dispatch<TasksReducerActionsType>, getState: () => RootState) => {
+		dispatch(setRequestStatusAC({status: 'loading'}))
+		const task = getState().tasks[todoListId].find((t) => t.id === taskForChangeId);
+		if (task) {
+			const model: UpdateTaskModelType = {
+				title: task.title,
+				deadline: task.deadline,
+				startDate: task.startDate,
+				priority: task.priority,
+				description: task.description,
+				status: newStatus
+			}
+
+			todoListsApi.updateTask(todoListId, taskForChangeId, model).then((res) => {
+				if (res.data.resultCode === ResultCode.OK) {
+					const action = changeTaskStatusAC({todoListId, taskForChangeId, newStatus})
+					dispatch(action)
+					dispatch(setRequestStatusAC({status: 'succeeded'}))
+				} else {
+					handleServerAppError<{ item: TaskFromServerType }>(dispatch, res.data)
+				}
+			}).catch((e) => {
+				handleServerNetworkError(dispatch, e)
+			})
+		}
+	}
+
+export const changeTaskTitleTC = (todoListId: string, taskForChangeId: string, newTitle: string) =>
+	async (dispatch: Dispatch<TasksReducerActionsType>, getState: () => RootState) => {
+		const task = getState().tasks[todoListId].find((t) => t.id === taskForChangeId);
+		if (task) {
+			const model: UpdateTaskModelType = {
+				title: newTitle,
+				deadline: task.deadline,
+				startDate: task.startDate,
+				priority: task.priority,
+				description: task.description,
+				status: task.status
+			}
+			try {
+				const res = await todoListsApi.updateTask(todoListId, taskForChangeId, model)
+				if (res.data.resultCode === ResultCode.OK) {
+					const action = changeTaskTitleAC({todoListId, taskForChangeId, newTitle})
+					dispatch(action)
+					dispatch(setRequestStatusAC({status: 'succeeded'}))
+				}
+			} catch (e: any) {
+				handleServerNetworkError(dispatch, e)
+			}
+		}
+	}
+
+//types:
+export type TasksForTodoListType = {
+	[key: string]: TaskFromServerType[]
+}
+
+type AddTaskActionType = ReturnType<typeof addTaskAC>//автоматически типизируем объект, который вернет экшн криэйтор addTaskAC
+//type RemoveTaskActionType = ReturnType<typeof removeTaskAC> //автоматически типизируем объект, который вернет экшн криэйтор removeTaskAC
+type ChangeTaskStatusActionType = ReturnType<typeof changeTaskStatusAC>
+type ChangeTaskTitleActionType = ReturnType<typeof changeTaskTitleAC>
+//type SetTasksActionType = ReturnType<typeof setTasksAC>
+//Тип экшенов для TasksReducer (можно не выносить типы в отдельные переменные, а сразу в TasksReducerActionsType писать ReturnType<typeof ...> :
+export type TasksReducerActionsType =
+	//| RemoveTaskActionType
+	| AddTaskActionType
+	| ChangeTaskStatusActionType
+	| ChangeTaskTitleActionType
+	| AddTodoListActionType
+	| RemoveTodoListActionType
+	| setTodoListsActionType
+	//| SetTasksActionType
+	| SetRequestStatusType
+	| SetRequestErrorType
+
+
+
+
 
 // export const _tasksReducer = (state: TasksForTodoListType = initialState, action: any): TasksForTodoListType => {
 // 	switch (action.type) {
@@ -184,133 +357,6 @@ export const {
 //
 // export const setTasksAC = (todoListId: string, tasks: TaskFromServerType[]) =>
 //     ({type: 'SET-TASKS', payload: {todoListId, tasks}} as const)
-
-//thunkCreators:
-export const fetchTasksTC = (todoListId: string) => (dispatch: Dispatch) => {
-	dispatch(setRequestStatusAC({status: 'loading'}))
-	todoListsApi.getTasks(todoListId)
-		.then((res) => {
-			dispatch(setTasksAC({todoListId, tasks: res.data.items}))
-			dispatch(setRequestStatusAC({status: 'succeeded'}))
-		}).catch((e) => {
-		handleServerNetworkError(dispatch, e)
-	})
-}
-
-export const addTaskTC = (todoListId: string, title: string) => (dispatch: Dispatch) => {
-	dispatch(setRequestStatusAC({status: 'loading'}))
-	todoListsApi.createTask(todoListId, title)
-		.then((res) => {
-			if (res.data.resultCode === ResultCode.OK) {
-				const task = res.data.data.item
-				const action = addTaskAC({todoListId, task})//можно todoListId не передавать, т.к. он есть в свойствах у таски с сервера
-				dispatch(action)
-				dispatch(setRequestStatusAC({status: 'succeeded'}))
-				dispatch(setRequestStatusAC({status: 'idle'}))
-			} else {
-				handleServerAppError<{ item: TaskFromServerType }>(dispatch, res.data)
-			}
-		}).catch((e) => {
-		handleServerNetworkError(dispatch, e)
-	})
-}
-
-export const removeTaskTC = (todoListId: string, taskForRemoveId: string) => (dispatch: Dispatch<TasksReducerActionsType>) => {
-	dispatch(setRequestStatusAC({status: 'loading'}))
-	todoListsApi.deleteTask(todoListId, taskForRemoveId)
-		.then((res) => {
-			if (res.data.resultCode === ResultCode.OK) {
-				dispatch(removeTaskAC({todoListId, taskForRemoveId}))
-				dispatch(setRequestStatusAC({status: 'succeeded'}))
-			} else {
-				handleServerAppError(dispatch, res.data)
-			}
-		}).catch((e) => {
-		handleServerNetworkError(dispatch, e)
-	})
-}
-
-export const changeTaskStatusTC = (todoListId: string, taskForChangeId: string, newStatus: TaskStatuses) =>
-	(dispatch: Dispatch<TasksReducerActionsType>, getState: () => RootState) => {
-		dispatch(setRequestStatusAC({status: 'loading'}))
-		const task = getState().tasks[todoListId].find((t) => t.id === taskForChangeId);
-		if (task) {
-			const model: UpdateTaskModelType = {
-				title: task.title,
-				deadline: task.deadline,
-				startDate: task.startDate,
-				priority: task.priority,
-				description: task.description,
-				status: newStatus
-			}
-
-			todoListsApi.updateTask(todoListId, taskForChangeId, model).then((res) => {
-				if (res.data.resultCode === ResultCode.OK) {
-					const action = changeTaskStatusAC({todoListId, taskForChangeId, newStatus})
-					dispatch(action)
-					dispatch(setRequestStatusAC({status: 'succeeded'}))
-				} else {
-					handleServerAppError<{ item: TaskFromServerType }>(dispatch, res.data)
-				}
-			}).catch((e) => {
-				handleServerNetworkError(dispatch, e)
-			})
-		}
-	}
-
-export const changeTaskTitleTC = (todoListId: string, taskForChangeId: string, newTitle: string) =>
-	async (dispatch: Dispatch<TasksReducerActionsType>, getState: () => RootState) => {
-		const task = getState().tasks[todoListId].find((t) => t.id === taskForChangeId);
-		if (task) {
-			const model: UpdateTaskModelType = {
-				title: newTitle,
-				deadline: task.deadline,
-				startDate: task.startDate,
-				priority: task.priority,
-				description: task.description,
-				status: task.status
-			}
-			try {
-				const res = await todoListsApi.updateTask(todoListId, taskForChangeId, model)
-				if (res.data.resultCode === ResultCode.OK) {
-					const action = changeTaskTitleAC({todoListId, taskForChangeId, newTitle})
-					dispatch(action)
-					dispatch(setRequestStatusAC({status: 'succeeded'}))
-				}
-			} catch (e: any) {
-				handleServerNetworkError(dispatch, e)
-			}
-		}
-	}
-
-//types:
-export type TasksForTodoListType = {
-	[key: string]: TaskFromServerType[]
-}
-
-type AddTaskActionType = ReturnType<typeof addTaskAC>//автоматически типизируем объект, который вернет экшн криэйтор addTaskAC
-type RemoveTaskActionType = ReturnType<typeof removeTaskAC> //автоматически типизируем объект, который вернет экшн криэйтор removeTaskAC
-type ChangeTaskStatusActionType = ReturnType<typeof changeTaskStatusAC>
-type ChangeTaskTitleActionType = ReturnType<typeof changeTaskTitleAC>
-type SetTasksActionType = ReturnType<typeof setTasksAC>
-//Тип экшенов для TasksReducer (можно не выносить типы в отдельные переменные, а сразу в TasksReducerActionsType писать ReturnType<typeof ...> :
-export type TasksReducerActionsType =
-	| RemoveTaskActionType
-	| AddTaskActionType
-	| ChangeTaskStatusActionType
-	| ChangeTaskTitleActionType
-	| AddTodoListActionType
-	| RemoveTodoListActionType
-	| setTodoListsActionType
-	| SetTasksActionType
-	| SetRequestStatusType
-	| SetRequestErrorType
-
-
-
-
-
-
 
 
 
